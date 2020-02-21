2f13ca06bbee99051f841ff188ee827b1ecb0280
vjmarcus <vjmarcus1@gmail.com>
2020-02-21 15:10:50 +0300

    Kotlin is fun!

--- app/src/main/java/com/freshappbooks/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/freshappbooks/firstkotlin/MainActivity.kt
@@ -2,21 +2,22 @@ package com.freshappbooks.firstkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var editText: EditText
-    private lateinit var buttonSave: Button
-    private lateinit var textViewName: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        editText = findViewById(R.id.editText)
-        buttonSave = findViewById(R.id.buttonSave)
-        textViewName = findViewById(R.id.textViewName)
+        buttonSave.setOnClickListener {
+            val name: String = editText.text.toString().trim()
+            textViewName.text = "Привет, $name"
+        }
     }
 }
